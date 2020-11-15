68fdba64b5bb39711d7b992e8bfc98b7749f545a
schoenlebm <43391573+schoenlebm@users.noreply.github.com>
2020-11-15 01:11:34 -0500

    added views functions for disaster and air quality (finished) and a grade converstion helper

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crime/grade', views.get_crime_grade, name="get_crime_grade")
+    path('crime/grade', views.get_crime_grade, name="get_crime_grade"),
+	path('airquality/grade',views.get_air_quality,name ="get_air_quality"),
+	path('weather/grade',views.get_weather_grade,name = "get_weather_grade"),
+	path('disaster/grade',views.get_disaster_grade, name = "get_disaster_grade"),
+
+
 ]
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 import random
+import requests
+import math
 
 def get_crime_grade(request):
     if request.method == "GET":
@@ -10,3 +12,47 @@ def get_crime_grade(request):
             "county": county,
             "crime_grade": random.randint(0,4) # A = 0, F = 4
         })
+
+def get_air_quality(request):
+	if request.method == "GET":
+		lon = request.GET['lon']
+		lat = request.GET['lat']
+		aq_request = requests.get("https://api.breezometer.com/air-quality/v2/current-conditions?lat="+ lat + "&lon=" + lon + "&key=f2e0d678257a4a9694ff2793afb4df61")
+		aqi = aq_request.json()['data']['indexes']['baqi']['aqi']
+		aqc = aq_request.json()['data']['indexes']['baqi']['category']
+
+		aq_grade = conv_grade_to_number(aqi)
+		return JsonResponse({
+			"lat": lat,
+			"lon": lon,
+			"air_quality_grade": aqn_grade,
+		})
+
+def get_disaster_grade(request):
+	if request.method == "GET":
+		state = request.GET['state']
+		#num_disasters = 
+		return JsonResponse({
+			"lat": lat,
+			"lon": lon,
+			"disaster_grade": aq_grade,
+		})
+		
+def conv_grade_to_number(p_grade):
+	p_grade = p_grade/10
+	p_grade = math.floor(p_grade)
+	if p_grade <= 5:
+		n_grade = 0
+	else:
+		n_grade = p_grade - 5;
+	return n_grade
+
+#def get_weather_grade(request):
+#	if request.method == "GET"
+#		state = request.GET['state']
+#       county = request.GET['county']
+#		zip_code = request.GET['zip_code']
+
+
+
+		
